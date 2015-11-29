dc1e38db51803ff15b5156f2d8c8f874f928b505
Kitakami Ooi <kitakami@ooi.moe>
2015-11-29 19:29:48 +0800

    网站的基本前端框架和登录表单

--- base/config.py
+++ base/config.py
@@ -2,3 +2,6 @@ import os
 
 # 代理服务器
 proxy = os.environ.get('OOI_PROXY', None)
+
+# Cookie的secret key
+secret_key = os.environ.get('OOI_SECRET_KEY', 'You Must Set A Secret Key!').encode()
--- ooi.py
+++ ooi.py
@@ -2,8 +2,16 @@
 
 import argparse
 import asyncio
-from aiohttp import web
+import os
 
+import jinja2
+from aiohttp import web
+import aiohttp_jinja2
+from aiohttp_session import session_middleware
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
+
+from base import config
+from handlers.frontend import FrontEndHandler
 from handlers.service import ServiceHandler
 
 parser = argparse.ArgumentParser(description='Online Objects Integration version 3.0')
@@ -11,6 +19,8 @@ parser.add_argument('-H', '--host', default='127.0.0.1',
                     help='The host of OOI server')
 parser.add_argument('-p', '--port', type=int, default=9999,
                     help='The port of OOI server')
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
@@ -28,14 +38,23 @@ def main():
     loop = asyncio.get_event_loop()
 
     # 初始化请求处理器
+    frontend = FrontEndHandler()
     service = ServiceHandler()
 
+    # 定义会话中间件
+    middlewares = [session_middleware(EncryptedCookieStorage(config.secret_key)), ]
+
     # 初始化应用
-    app = web.Application(loop=loop)
+    app = web.Application(middlewares=middlewares, loop=loop)
+
+    # 定义Jinja2模板位置
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(os.path.join(base_dir, 'templates')))
 
     # 给应用添加路由
+    app.router.add_route('GET', '/', frontend.form)
     app.router.add_route('POST', '/service/osapi', service.get_osapi)
     app.router.add_route('POST', '/service/flash', service.get_flash)
+    app.router.add_static('/static', os.path.join(base_dir, 'static'))
     app_handlers = app.make_handler()
 
     # 启动OOI服务器
