05f7032752956730ae1a77b192106fe36f1f3dc0
Kitakami Ooi <kitakami@ooi.moe>
2015-12-12 22:56:54 +0800

    缓存镇守府的图片和api_start2的结果

--- handlers/api.py
+++ handlers/api.py
@@ -23,6 +23,10 @@ class APIHandler:
         else:
             self.connector = None
 
+        # 初始化存放镇守府图片和api_start2内容的变量
+        self.api_start2 = None
+        self.worlds = {}
+
     async def world_image(self, request):
         """ 显示正确的镇守府图片。
         舰娘游戏中客户端FLASH请求的镇守府图片是根据FLASH本身的URL生成的，需要根据用户所在的镇守府IP为其显示正确的图片。
@@ -35,14 +39,18 @@ class APIHandler:
         world_ip = session['world_ip']
         if world_ip:
             ip_sections = map(int, world_ip.split('.'))
-            world_ip = '_'.join([format(x, '03') for x in ip_sections])
-            url = 'http://203.104.209.102/kcs/resources/image/world/' + world_ip + '_' + size + '.png'
-            coro = aiohttp.get(url, connector=self.connector)
-            try:
-                response = await asyncio.wait_for(coro, timeout=5)
-            except asyncio.TimeoutError:
-                return aiohttp.web.HTTPBadRequest()
-            body = await response.read()
+            image_name = '_'.join([format(x, '03') for x in ip_sections]) + '_' + size
+            if image_name in self.worlds:
+                body = self.worlds[image_name]
+            else:
+                url = 'http://203.104.209.102/kcs/resources/image/world/' + image_name + '.png'
+                coro = aiohttp.get(url, connector=self.connector)
+                try:
+                    response = await asyncio.wait_for(coro, timeout=5)
+                except asyncio.TimeoutError:
+                    return aiohttp.web.HTTPBadRequest()
+                body = await response.read()
+                self.worlds[image_name] = body
             return aiohttp.web.Response(body=body, headers={'Content-Type': 'image/png'})
         else:
             return aiohttp.web.HTTPBadRequest()
@@ -57,23 +65,29 @@ class APIHandler:
         session = await get_session(request)
         world_ip = session['world_ip']
         if world_ip:
-            referrer = request.headers.get('REFERER')
-            referrer = referrer.replace(request.host, world_ip)
-            referrer = referrer.replace('https://', 'http://')
-            url = 'http://' + world_ip + '/kcsapi/' + action
-            headers = aiohttp.MultiDict({
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
-                'Origin': 'http://' + world_ip + '/',
-                'Referer': referrer,
-                'X-Requested-With': 'ShockwaveFlash/18.0.0.232'
-            })
-            data = await request.post()
-            coro = aiohttp.post(url, data=data, headers=headers, connector=self.connector)
-            try:
-                response = await asyncio.wait_for(coro, timeout=5)
-            except asyncio.TimeoutError:
-                return aiohttp.web.HTTPBadRequest()
-            body = await response.read()
-            return aiohttp.web.Response(body=body, headers={'Content-Type': response.headers['CONTENT-TYPE']})
+            if action == 'api_start2' and self.api_start2 is not None:
+                return aiohttp.web.Response(body=self.api_start2,
+                                            headers=aiohttp.MultiDict({'Content-Type': 'text/plain'}))
+            else:
+                referrer = request.headers.get('REFERER')
+                referrer = referrer.replace(request.host, world_ip)
+                referrer = referrer.replace('https://', 'http://')
+                url = 'http://' + world_ip + '/kcsapi/' + action
+                headers = aiohttp.MultiDict({
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+                    'Origin': 'http://' + world_ip + '/',
+                    'Referer': referrer,
+                    'X-Requested-With': 'ShockwaveFlash/18.0.0.232'
+                })
+                data = await request.post()
+                coro = aiohttp.post(url, data=data, headers=headers, connector=self.connector)
+                try:
+                    response = await asyncio.wait_for(coro, timeout=5)
+                except asyncio.TimeoutError:
+                    return aiohttp.web.HTTPBadRequest()
+                body = await response.read()
+                if action == 'api_start2':
+                    self.api_start2 = body
+                return aiohttp.web.Response(body=body, headers=aiohttp.MultiDict({'Content-Type': 'text/plain'}))
         else:
             return aiohttp.web.HTTPBadRequest()
